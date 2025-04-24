77a63f52dde5534b3000a2fae5cc5a553997499a
xzgao <xz.gao@connect.ust.hk>
2025-04-24 12:21:26 +0000

    update

--- jcode/benchmark_tropical.jl
+++ jcode/benchmark_tropical.jl
@@ -3,50 +3,41 @@ using Graphs, GraphIO
 using CUDA, LinearAlgebra
 using CSV, DataFrames
 using TropicalNumbers, CuTropicalGEMM
+using GenericTensorNetworks
 
-function generate_tensors(g)
-    tensors = []
+function benchmark(id::Int, sc)
+    graph = loadgraph("../networks/sc$(sc)/graph_$id.dot")
+    code = readjson("../networks/sc$(sc)/eincode_$id.json")
 
-    for i in 1:ne(g)
-        push!(tensors, rand(Float32, (2, 2)))
-    end
-
-    for j in 1:nv(g)
-        push!(tensors, rand(Float32, 2))
-    end
-
-    return Tropical.(CuArray.(tensors))
-end
-
-function benchmark(id::Int)
-    graph = loadgraph("../networks/graph_$id.dot")
-    code = readjson("../networks/eincode_$id.json")
-
-    tensors = generate_tensors(graph)
+    tn = GenericTensorNetwork(IndependentSet(graph), code, Dict{Int, Int}())
+    @show contraction_complexity(tn)
 
     # warmup
-    res = code(tensors...)
-    @show id, Array(res)
+    res = solve(tn, SizeMax(), T = Float32, usecuda = true)
+    @show id, Array(res)[]
 
     # 10 times runs for benchmark
     t = @elapsed begin
-        for i in 1:10
-            code(tensors...)
+        for i in 1:5
+            @show id, i
+            solve(tn, SizeMax(), T = Float32, usecuda = true)
         end
     end
 
-    return t / 10
+    @show id, t / 5
+
+    return t / 5, Array(res)[]
 end
 
-function main()
-    df = CSV.write("../data/benchmark_julia_tropical.csv", DataFrame(id = Int[], time = Float64[]))
+function main(sc)
+    df = CSV.write("../data/benchmark_julia_tropical_sc$(sc).csv", DataFrame(id = Int[], time = Float64[], res = []))
 
     for id in 1:10
-        t = benchmark(id)
-        CSV.write(df, DataFrame(id = [id], time = [t]), append = true)
+        t, res = benchmark(id, sc)
+        CSV.write(df, DataFrame(id = [id], time = [t], res = [res.n]), append = true)
     end
 
     return df
 end
 
-main()
+main(31)
